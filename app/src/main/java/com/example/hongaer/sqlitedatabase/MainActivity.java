c624c5868483d4a8219c351e88b709d564d9afdd
http://git.oschina.net/madness <736036724@qq.com>
2017-06-20 11:04:15 +0800

    mainactivity 2.0

--- app/src/main/java/com/example/hongaer/sqlitedatabase/MainActivity.java
+++ app/src/main/java/com/example/hongaer/sqlitedatabase/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.hongaer.sqlitedatabase;
 
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -28,6 +32,51 @@ public class MainActivity extends AppCompatActivity {
         db = sqliteopenhelper_.getReadableDatabase();
         btn_insert.setOnClickListener(onClicklistener);
         btn_query.setOnClickListener(onClicklistener);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, final View view, int position, long id) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("请选择");
+                builder.setItems(R.array.dialog_arry, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case 0:
+                                LayoutInflater inflater=getLayoutInflater().from(MainActivity.this);
+                                View view=inflater.inflate(R.layout.dialog_item,null);
+                                EditText dialog_classname= (EditText) view.findViewById(R.id.dialog_classname);
+                                EditText dialog_classtype= (EditText) view.findViewById(R.id.dialog_classtype);
+                                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                                builder.setTitle("请更改");
+                                builder.setView(view);
+                                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                });
+                                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                });
+                                builder.show();
+                             Toast.makeText(MainActivity.this,"点击了更改",Toast.LENGTH_LONG).show();
+
+                            break;
+
+                            case 1:
+                             Toast.makeText(MainActivity.this,"点击了删除",Toast.LENGTH_LONG).show();
+                            break;
+
+                        }
+                    }
+                });
+                 builder.show();
+                return false;
+            }
+        });
     }
 
     private View.OnClickListener onClicklistener = new View.OnClickListener() {
